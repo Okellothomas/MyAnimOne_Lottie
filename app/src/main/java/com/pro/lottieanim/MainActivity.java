4e29773b0de61616bbf0229483769d621468169c
Okellothomas <thomasolal33@gmail.com>
2022-07-16 23:08:29 +0300

    remove action bar from the main activity

--- app/src/main/java/com/pro/lottieanim/MainActivity.java
+++ app/src/main/java/com/pro/lottieanim/MainActivity.java
@@ -2,7 +2,9 @@ package com.pro.lottieanim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -26,7 +28,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         secondone.setOnClickListener(this);
 
         // we then move to the next activity after 4 seconds
+        Handler handler = new Handler();
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // we define our intention
+                Intent intent = new Intent(getApplicationContext(), Onboarding.class);
+                startActivity(intent);
+                finish();
+            }
+        },6000);
     }
 
     @Override
